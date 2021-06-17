5097496c13643592e999aa2c771ce84fde64cda5
Pilinger György Ivó <pilinger.gy@gmail.com>
2021-06-17 21:03:30 +0200

    tenbelow.py modified clean code

--- python-homework/tenbelow.py
+++ python-homework/tenbelow.py
@@ -1,12 +1,11 @@
 
-beolvas = True
-sum = 0
+read_in = True
+summary = 0
 
-while beolvas:
+while read_in:
     num = int(input('Kérem adjon meg egy egész számot = '))
     if num < 10:
-        sum += num
+        summary += num
     else:
-        print('\n' + 'A bekért 10-nél kisebb számok összege =', sum)
-        beolvas = False
-
+        print('\n' + 'A bekért 10-nél kisebb számok összege =', summary)
+        read_in = False
