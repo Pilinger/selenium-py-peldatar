9365730a46a9ca94bdc233bea4bc89c86d7f3d59
Pilinger György Ivó <pilinger.gy@gmail.com>
2021-06-23 20:16:44 +0200

    fajl1.py and adat.txt added

---
+++ python2-homework/fajl1.py
@@ -0,0 +1,4 @@
+with open('adat.txt', 'r') as f:
+    print(f.readline().strip(), f.readline().strip(), f.readline().strip(), f.readline().strip(),
+          f.readline().strip(), f.readline().strip(), f.readline().strip(),
+          f.readline().strip(), f.readline().strip())
