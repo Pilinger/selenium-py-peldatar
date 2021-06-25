71fc14469e5cca37382c6f5d8d58dd4dfd66f473
Pilinger György Ivó <pilinger.gy@gmail.com>
2021-06-25 17:03:26 +0200

    fajl2.py modified

--- python2-homework/fajl2.py
+++ python2-homework/fajl2.py
@@ -2,4 +2,12 @@ with open('adat.txt', 'r') as f:
     l_adat = []
     for line in f:
         l_adat.append(line)
-    print(l_adat)
+
+once_str = ''
+for s in l_adat:
+    if l_adat.index(s) == 0:
+        once_str += s.strip()
+    else:
+        once_str += ' ' + s.strip()
+
+print(once_str)
