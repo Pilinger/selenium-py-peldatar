0e35aa8dcd486f3b9460fb8f03d300a1f6f4fa08
Pilinger György Ivó <pilinger.gy@gmail.com>
2021-06-25 17:44:54 +0200

    fajl3.py added

---
+++ python2-homework/fajl3.py
@@ -0,0 +1,17 @@
+with open('adat.txt', 'r') as f:
+    l_adat = []
+    for line in f:
+        l_adat.append(line)
+
+once_str = ''
+for s in l_adat:
+    if l_adat.index(s) == 0:
+        once_str += s.strip()
+    else:
+        once_str += ' ' + s.strip()
+
+with open('one_line.txt', 'w') as f_w:
+    f_w.write(once_str)
+
+with open('one_line.txt', 'r') as f_r:
+    print(f_r.readline())
